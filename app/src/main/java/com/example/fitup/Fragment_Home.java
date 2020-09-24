2704cce7a97c5863f1b0d634e78cbe2f816fc08c
LiranFain22 <lirandeveloper@gmail.com>
2020-09-24 17:21:21 +0300

    working in progress

--- app/src/main/java/com/example/fitup/Fragment_Home.java
+++ app/src/main/java/com/example/fitup/Fragment_Home.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +13,28 @@ import androidx.fragment.app.Fragment;
 
 public class Fragment_Home extends Fragment {
 
+    private TextView Fragment_home_userTitle;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        findViews(view);
+    }
+
+    private void findViews(View view) {
+        Fragment_home_userTitle = view.findViewById(R.id.Fragment_home_userTitle);
     }
 }
